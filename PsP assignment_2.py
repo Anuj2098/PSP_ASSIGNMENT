ff448a8163fd03f657645612086b0cf003cd6d25
Anuj2098 <78247183+Anuj2098@users.noreply.github.com>
2021-02-16 23:10:09 +0530

    Add files via upload

---
+++ PsP assignment_2.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+
+heads=0
+tails=0
+print("Assingment_2 M.Tech CS")
+print("Sample space={Heads, Tails}")
+flip=int(input("how many times, you want to toss the coin:"))
+
+for i in range(flip):
+    result=randint(0,1)
+    sleep(1)
+    if result==0:
+         print("Heads")
+         heads+=1
+    else:
+        print("Tails")
+        tails+=1
+
+print("The results are :")
+print("Heads",heads)
+print("Tails",tails)
---
+++ PsP assignment_2_output.py
@@ -0,0 +1,21 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Lenovo/AppData/Local/Programs/Python/Python37-32/PsP assignment_2.py 
+Assingment_2 M.Tech CS
+Sample space={Heads, Tails}
+how many times, you want to toss the coin:10
+Heads
+Heads
+Tails
+Heads
+Heads
+Tails
+Heads
+Heads
+Heads
+Tails
+The results are :
+Heads 7
+Tails 3
+>>> 
